fa9ca56f7b9222ef84db80e98a65fe1fffed3a2f
Max Steinhausen <max.steinhausen@tu-braunschweig.de>
2024-10-23 11:27:03 +0200

    change some python keymappings

--- lua/plugins/py-repl.lua
+++ lua/plugins/py-repl.lua
@@ -1,3 +1,18 @@
 return {
-     { "geg2102/nvim-python-repl" },
+  { "geg2102/nvim-python-repl" },
+  -- opts = {
+  --   execute_on_send = true,
+  --   vsplit = false,
+  --   prompt_spawn = false,
+  --   spawn_command = {
+  --     python = "python",
+  --   },
+  --   keys = {
+  --     {
+  --       "<leader>p",
+  --       ":lua require('nvim-python-repl').send_statement_definition()<cr>",
+  --       desc = "Send semantic unit to REPL",
+  --     },
+  --   },
+  -- },
 }
