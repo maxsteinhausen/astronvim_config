829360d90d3fe4f40a2449087bff3db696fc3b2a
Max Steinhausen <max.steinhausen@tu-braunschweig.de>
2025-01-05 11:38:15 +0100

    add some new plugins

--- lua/plugins/py-repl.lua
+++ lua/plugins/py-repl.lua
@@ -1,18 +1,13 @@
 return {
-  { "geg2102/nvim-python-repl" },
-  -- opts = {
-  --   execute_on_send = true,
-  --   vsplit = false,
-  --   prompt_spawn = false,
-  --   spawn_command = {
-  --     python = "python",
-  --   },
-  --   keys = {
-  --     {
-  --       "<leader>p",
-  --       ":lua require('nvim-python-repl').send_statement_definition()<cr>",
-  --       desc = "Send semantic unit to REPL",
-  --     },
-  --   },
-  -- },
+  {
+    "geg2102/nvim-python-repl",
+    dependencies = "nvim-treesitter",
+    ft = { "python", "lua", "scala" },
+    config = function()
+      require("nvim-python-repl").setup {
+        execute_on_send = false,
+        vsplit = false,
+      }
+    end,
+  },
 }
