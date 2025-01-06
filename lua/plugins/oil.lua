dcbe711cd98abbd8544b982eee0c85a5f210d5a5
Max Steinhausen <max.steinhausen@tu-braunschweig.de>
2025-01-06 16:36:57 +0100

    add mapping for oil file explorer

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,5 +7,8 @@ return {
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    keys = {
+      { "<leader>i", "<cmd>Oil<cr>", desc = "Open current directory in Oil" },
+    },
   },
 }
