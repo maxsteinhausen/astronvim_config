497f8741adc9c1c64e2be569d7c238c4c76c0807
Max Steinhausen <max.steinhausen@tu-braunschweig.de>
2025-01-06 16:51:12 +0100

    Oil add comments

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,8 +5,10 @@ return {
     ---@type oil.SetupOpts
     opts = {},
     -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    -- use nvim . in the terminal to open the current directory in Oil
+    -- use enter to go into folders and - to go back
     keys = {
       { "<leader>i", "<cmd>Oil --float<cr>", desc = "Open current directory in Oil" },
     },
