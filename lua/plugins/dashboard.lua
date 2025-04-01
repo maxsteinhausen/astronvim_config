27d386df8b69e408e34217c149c7ebab1d82f0d2
Max Steinhausen <max.steinhausen@tu-braunschweig.de>
2025-04-01 13:53:56 +0200

    make changes to new dashboard with snacks

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,34 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = table.concat({
+          [[   █  █   ]],
+          [[   █ ██   ]],
+          [[   ████   ]],
+          [[   ██ ███   ]],
+          [[   █  █   ]],
+          [[             ]],
+          [[ N E O V I M ]],
+        }, "\n"),
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          -- { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "u", desc = "Update AstroNvim", action = "<leader> pa" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
+  },
+}
